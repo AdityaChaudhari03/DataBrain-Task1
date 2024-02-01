eb2d4ae1c3a4a49d5e4abefc0498399a51645135
AdityaChaudhari03 <chaudhariaditya664@gmail.com>
2024-02-01 10:09:26 +0530

    Address Component Update

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { updateAddress } from '../redux/formSlice';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { updateAddress } from "../redux/formSlice";
 
 const Address = ({ onNext }) => {
   const { register, handleSubmit } = useForm();
@@ -13,41 +13,50 @@ const Address = ({ onNext }) => {
   };
 
   return (
-    <div className='translate-y-20'>
-      <div className='text-center font-bold mb-5 text-2xl'>
-        <h1>
-          Address Details
-        </h1>
+    <div className="translate-y-20">
+      <div className="text-center font-bold mb-5 text-2xl">
+        <h1>Address Details</h1>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto bg-gray-100 p-6 rounded-md ">
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">
-          Street:
-        </label>
-        <input {...register('street', { required: true })} className="mt-1 p-2 w-full border rounded-md text-black" />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">
-          City:
-        </label>
-        <input {...register('city', { required: true })} className="mt-1 p-2 w-full border rounded-md text-black" />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600 ">
-          Zip Code:
-        </label>
-        <input
-          {...register('zipCode', { required: true, pattern: /^\d{5}$/ })}
-          className="mt-1 p-2 w-full border rounded-md text-black"
-        />
-      </div>
-      <button type="submit" className="bg-blue-500 text-white w-20 p-2 rounded-md">
-        Next
-      </button>
-    </form>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="max-w-md mx-auto bg-gray-100 p-6 rounded-md "
+      >
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">
+            Street:
+          </label>
+          <input
+            {...register("street", { required: true })}
+            className="mt-1 p-2 w-full border rounded-md text-black"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">
+            City:
+          </label>
+          <input
+            {...register("city", { required: true })}
+            className="mt-1 p-2 w-full border rounded-md text-black"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600 ">
+            Zip Code:
+          </label>
+          <input
+            {...register("zipCode", { required: true, pattern: /^\d{6}$/ })}
+            className="mt-1 p-2 w-full border rounded-md text-black"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white w-20 p-2 rounded-md"
+        >
+          Next
+        </button>
+      </form>
     </div>
   );
 };
-
 
 export default Address;
